50a6df35db4c406e98c0c8b70bdaff006b54b04d
greenfrog <greenfrog82@naver.com>
2018-03-15 18:23:23 +0900

    Some codes had been modifed to work well in multi-templates.

--- src/src/index.js
+++ src/src/index.js
@@ -2,34 +2,48 @@ import preProcess from './pre_processor'
 import process from './processor'
 import { spawn } from 'child_process'
 
-const RESOURCE_PATH = {
+const resourcesPath = {
   app: '/develop/src/sample_workspace/app/resources/app',
   shared:'/develop/src/sample_workspace/shared/resources/shared'
 }
 
-const TEMPLATE_PATH = {
+const templatesPath = {
   app: '/develop/src/sample_workspace/app/templates',
   shared: '/develop/src/sample_workspace/shared/templates',
 }
 
-const grep = spawn('grep',['-Pro', "[\"|\'](/static/.+?)[\"|\']", TEMPLATE_PATH.app]);
+const promises = [];
+const searchReport = {};
 
-let resGrep = '';
-const report = {};
+for(let key in templatesPath) {
+  promises.push(new Promise(resolve => {
+    const grep = spawn('grep',['-Pro', "[\"|\'](/static/.+?)[\"|\']", templatesPath[key]]);
 
-grep.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-  resGrep += data;
-});
+    const report = {};
+    let resGrep = '';
 
-grep.stdout.on('close', (code) => {
-  console.log(`grep process exited with code ${code}`);
-  const repo = preProcess(resGrep)
-  process(repo, RESOURCE_PATH, report, 'app');
-  console.log('>>> ', report);
+    grep.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+      resGrep += data;
+    });
 
-});
+    grep.stdout.on('close', (code) => {
+      console.log(`grep process exited with code ${code}`);
+      const repo = preProcess(resGrep)
+      process(repo, resourcesPath, report, key);
+      
+      searchReport[key] = report;
+      resolve(report);
+    });
 
-grep.stdout.on('error', (err) => {
-  console.log(`Error ${err}`);
-});
+    grep.stdout.on('error', (err) => {
+      console.log(`Error ${err}`);
+    });
+  }));
+}
+
+Promise.all(promises)
+.then(param => {
+  console.log(param);
+  console.log(searchReport);
+})
